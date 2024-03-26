22fd59a13b9bd455d2cf6286cbd723edae505ee1
Stephen Onyango <onyangosteve01@gmail.com>
2024-03-26 14:48:38 +0300

    adds .find method to the code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+
+function superbowlWin(array){
+  if (array.result === "W"){
+    return array.year
+  }else{
+    return undefined
+  }
+}
+
+console.log(record.find(superbowlWin));
